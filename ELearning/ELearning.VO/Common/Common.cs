e2421973d408d5bf14ac61b0a1fa21de13b08d02
BANDINI <bandini.bhopi@gmail.com>
2016-09-21 16:50:00 -0700

    Project Skeleton is ready.

---
+++ ELearning/ELearning.VO/Common/Common.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELearning.VO.Common
+{
+    class Common
+    {
+        public string FormatString(string strString)
+        {
+            if (strString == null)
+                return string.Empty;
+            else
+                return strString;
+        }
+    }
+}
